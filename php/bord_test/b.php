f8d7717879da2dcb445a0813b8b069aada1eda67
juuuuunii <01071720722s@gmail.com>
2018-11-13 11:50:01 +0900

    181113

---
+++ php/bord_test/b.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>호뮤ㅔ이지</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	<div class="wrap">
+	
+	</div>
+</body>
+</html>
---
+++ php/inc/header.php
@@ -0,0 +1,17 @@
+
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="../../board_test/css/main.css">
+<div class="wrap">
+		<header class="header clear">
+			<div class="logo">SITE LOGO</div>
+			<div class="title">My HOME</div>
+			<div class="bar">
+				<span class="fa fa-bars"></span>
+			</div>			
+		</header>
+		<ul class="menus clear">
+			<li><a href="board.php?bo_table=notice2">공지사항</a></li>
+			<li><a href="board.php?bo_table=qna2">묻고답하기</a></li>
+			<li><a href="board.php?bo_table=photo2">갤러리</a></li>
+		</ul>
+		<div class="board">
